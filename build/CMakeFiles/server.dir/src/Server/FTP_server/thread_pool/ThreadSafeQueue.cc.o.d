4718133f9c9839f3d870b86049ebfe292f6acbfd
linnaid <jiaguoyangyang@qq.com>
2025-08-21 00:08:11 +0800

    Delete build directory

--- build/CMakeFiles/server.dir/src/Server/FTP_server/thread_pool/ThreadSafeQueue.cc.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/server.dir/src/Server/FTP_server/thread_pool/ThreadSafeQueue.cc.o: \
- /home/linnaid/ChatRoom/chatroom_1/src/Server/FTP_server/thread_pool/ThreadSafeQueue.cc \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/server.dir/src/Server/sec-r_l.cc.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/server.dir/src/Server/sec-r_l.cc.o: \
- /home/linnaid/ChatRoom/chatroom_1/src/Server/sec-r_l.cc \
- /usr/include/stdc-predef.h
